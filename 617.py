3ce7f344b1925578165ab481693b43a87d161c66
xmu-ggx <1415606326@qq.com>
2019-12-19 14:30:16 +0800

    合并二叉树，同步遍历 ，递归

---
+++ 617.py
@@ -0,0 +1,16 @@
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def mergeTrees(self, t1, t2):
+        if t1 is not None and t2 is not None:
+            t1.val += t2.val
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+            return t1
+
+        return t1 if t2 is None else t2
